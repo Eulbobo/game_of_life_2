e45bae43d717047962ef916de63b60caa97f78ed
Julien Boniol <julien.boniol@adod.fr>
2024-08-20 09:09:00 +0200

    once again, dumbest way to implement

--- src/main/java/fr/eulbobo/dojo/gameoflife/domain/GameOfLife.java
+++ src/main/java/fr/eulbobo/dojo/gameoflife/domain/GameOfLife.java
@@ -4,20 +4,25 @@ import fr.eulbobo.dojo.gameoflife.domain.display.CellDisplay;
 import fr.eulbobo.dojo.gameoflife.domain.generator.CellData;
 import fr.eulbobo.dojo.gameoflife.domain.generator.CellGenerator;
 
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 
 public class GameOfLife {
 
-    Position position;
-    private GameOfLife(Position position) {
-       this.position = position;
-    }
+    Collection<Position> cells;
 
-    public void displayTo(CellDisplay displayer) {
-        displayer.alive(position);
+    private GameOfLife(Collection<Position> cells) {
+        this.cells = cells;
     }
 
     public static GameOfLife from(CellGenerator cellGenerator) {
-        Position position = cellGenerator.allCells().stream().findFirst().map(CellData::getPosition).get();
-        return new GameOfLife(position);
+        Set<Position> positions = cellGenerator.allCells().stream().map(CellData::getPosition).collect(Collectors.toSet());
+        return new GameOfLife(positions);
+    }
+
+    public void displayTo(CellDisplay displayer) {
+        cells.forEach(displayer::alive);
     }
 }
--- src/test/java/fr/eulbobo/dojo/gameoflife/domain/GameOfLifeTest.java
+++ src/test/java/fr/eulbobo/dojo/gameoflife/domain/GameOfLifeTest.java
@@ -3,36 +3,82 @@ package fr.eulbobo.dojo.gameoflife.domain;
 import fr.eulbobo.dojo.gameoflife.domain.display.CellDisplay;
 import fr.eulbobo.dojo.gameoflife.domain.generator.CellData;
 import fr.eulbobo.dojo.gameoflife.domain.generator.CellGenerator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import java.util.Set;
 
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 public class GameOfLifeTest {
 
     @Test
     void should_output_cells_to_displayer() {
-        CellDisplay displayer = Mockito.mock(CellDisplay.class);
+        CellDisplay displayer = mock(CellDisplay.class);
         GameOfLife gameOfLife = GameOfLife.from(singleAliveCellGenerator(2, 2));
 
         gameOfLife.displayTo(displayer);
 
-        Mockito.verify(displayer).alive(eq(new Position(2, 2)));
+        verify(displayer).alive(eq(new Position(2, 2)));
     }
 
     private CellGenerator singleAliveCellGenerator(int x, int y) {
-        return () -> Set.of(new CellData() {
-            @Override
-            public Position getPosition() {
-                return new Position(x, y);
-            }
+        return () -> Set.of(InnerCellData.alive(x, y));
+    }
 
-            @Override
-            public boolean isAlive() {
-                return true;
-            }
-        });
+    @Test
+    void should_output_more_cells_to_displayer() {
+        ArgumentCaptor<Position> captor = ArgumentCaptor.forClass(Position.class);
+        CellDisplay displayer = mock(CellDisplay.class);
+
+        GameOfLife gameOfLife = GameOfLife.from(manyAliveCellsGenerator());
+        gameOfLife.displayTo(displayer);
+
+        Mockito.verify(displayer, times(4)).alive(captor.capture());
+
+        Assertions.assertThat(captor.getAllValues())
+                .containsAll(Set.of(
+                        new Position(1, 1),
+                        new Position(1, 0),
+                        new Position(0, 0),
+                        new Position(0, 1))
+                );
+    }
+
+    private CellGenerator manyAliveCellsGenerator() {
+        return () -> Set.of(
+                InnerCellData.alive(1, 1),
+                InnerCellData.alive(1, 0),
+                InnerCellData.alive(0, 1),
+                InnerCellData.alive(0, 0));
+    }
+
+    private static class InnerCellData implements CellData {
+        private final boolean alive;
+        private final int x;
+        private final int y;
+
+        public InnerCellData(boolean alive, int x, int y) {
+            this.alive = alive;
+            this.x = x;
+            this.y = y;
+        }
+
+        public static InnerCellData alive(int x, int y) {
+            return new InnerCellData(true, x, y);
+        }
+
+        @Override
+        public Position getPosition() {
+            return new Position(x, y);
+        }
+
+        @Override
+        public boolean isAlive() {
+            return alive;
+        }
     }
 }
